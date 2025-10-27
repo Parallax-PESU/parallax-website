4c2b86cb7ad4db99a6f788559744ed5bdd9d14ba
Priyans00 <115811175+Priyans00@users.noreply.github.com>
2025-10-27 20:57:19 +0530

    feat(footer): add footer

---
+++ app/components/Footer.tsx
@@ -0,0 +1,11 @@
+export default function Footer(){
+    return (
+        <footer className="border-t border-[#0066ff]/20 py-8 sm:py-12 px-4 sm:px-6 lg:px-8 bg-[#050a15]/50">
+        <div className="w-full mx-auto text-center">
+          <p className="text-[#b0b0b0] text-sm sm:text-base ">
+            © 2025 Parallax Club - PESU Electronic City Campus
+          </p>
+        </div>
+      </footer>
+    )
+}
